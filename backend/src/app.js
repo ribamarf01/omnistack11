754ce175ea00c95294b6ef1e2f3c28eaf58be3f7
ribamarf01 <ribamar.f01@gmail.com>
2020-03-27 10:08:05 -0300

    Testes de backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const cors = require('cors')
+const { errors } = require('celebrate')
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
-app.listen(8000, () => {
-    console.log(`Executando na porta 8000`)
-})
+module.exports = app
